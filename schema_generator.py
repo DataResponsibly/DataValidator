42591036db1a2c18baede6f438e580e9cff74a6f
Jacob Versaggi <jcv42@drexel.edu>
2018-09-21 16:38:00 -0400

    Minor tweaks

--- schema_generator.py
+++ schema_generator.py
@@ -6,22 +6,66 @@ import os
 import pandas as pd
 import numpy as np
 
-'''
-For file attachments, the file should be a .json with the name <column>.json, in the format:
-{
-    <column>: [<parameters>]
-}
+"""
+An example of schema output:
 
-This script takes in a piece of metadata from the DataResponsibly Data Synthesizer
-and creates a Table Schema of the provided dataset, as detailed at https://frictionlessdata.io/specs/table-schema/
-'''
+{
+    "primaryKey": "",
+    "fields": [
+        {
+            "name": "key",
+            "type": "integer",
+            "constraints": {
+                "unique": true
+            }
+        },
+        {
+            "name": "zip",
+            "type": "string"
+        },
+        {
+            "name": "tract",
+            "type": "integer"
+        },
+        {
+            "name": "latitude",
+            "type": "number"
+        },
+        {
+            "name": "longitude",
+            "type": "number"
+        },
+        {
+            "name": "soc",
+            "type": "string",
+            "constraints": {
+                "unique": true,
+                "pattern": "[0-9]{3}-?[0-9]{2}-?[0-9]{4}"
+            }
+        }
+    ]
+}
+"""
 
 def schema_gen(input_file,attachments,path):
+    '''
+    This script takes in a simplified schema provided by the user, and changes it into a schema
+    that is recognizable by the goodtables library, which is defined at https://frictionlessdata.io/specs/table-schema/
+
+    input_file: the json description/schema file
+    attachments: a list of file names.
+        For file attachments, the file should be a .json with the name <column>.json, in the format:
+        {
+            <column>: [<parameters>]
+        }
+    path: directory of the input_file
+
+    '''
 
     if len(attachments) > 0: # Used to attach files in the format described above.
-        has_file = True
+        has_customized_domains = True
     else:
-        has_file = False
+        has_customized_domains = False
     if path != "":
         path = path + '/'
     schema = { 'primaryKey': '', 'fields' : [] }
@@ -32,17 +76,26 @@ def schema_gen(input_file,attachments,path):
         desc_json = json.loads(y)
         keys = desc_json['attribute_description'].keys()
         for key in keys:
+            is_ssn = False
             field = { 'name': '', 'type': '' }
             field['name'] = key
             type = desc_json['attribute_description'][key]['data_type'].lower()
+            if type == 'float':
+                type = 'number'
+            elif type == 'socialsecuritynumber':
+                type = 'string'
+                is_ssn = True
+
+
             field['type'] = type
-            if has_file:
+            if has_customized_domains:
                 filename = key + '.json'
                 if path + key + '.json' in attachments: # If a file is attached, creates enumerated list constraint for validation
                     with open(path + key +'.json') as sp:
                         json_raw = sp.read()
                         enums = json.loads(json_raw)
-                        field['constraints'] = {}
+                        if 'constraints' not in field.keys():
+                            field['constraints'] = {}
                         field['constraints']['enum'] = enums[key]
             if 'required' in desc_json['attribute_description'][key].keys() and 'unique' not in desc_json['attribute_description'][key].keys(): # Check for required constraint.
                 if desc_json['attribute_description'][key]['required']:
@@ -54,6 +107,23 @@ def schema_gen(input_file,attachments,path):
                     if 'constraints' not in field.keys():
                         field['constraints'] = {}
                     field['constraints']['unique'] = True
-            field_list.append(field)
-        schema['fields'] = field_list
+            '''
+            The pattern matching constraint of goodtables, even on the current
+            version, is slightly bugged. Due to a false error call, any calls
+            to the pattern matching constraint will immediately crash the program.
+
+            The fix is rather simple, where on the path denoted by goodtables/checks,
+            find the file pattern_constraint.py. Once found, go to line 19 and change
+            'cells.remove(error.cell)' to 'cells.remove(error._cell)'. While this seems
+            simple, the official github has yet to push this, or any other fix through.
+
+            More info can be found at: https://github.com/frictionlessdata/goodtables-py/issues/264
+            '''
+            if is_ssn:
+                if 'constraints' not in field.keys():
+                    field['constraints'] = {}
+                field['constraints']['pattern'] = '[0-9]{3}-?[0-9]{2}-?[0-9]{4}'
+
+            schema['fields'].append(field)
+        # schema['fields'] = field_list
     return schema
--- valitool.py
+++ valitool.py
@@ -11,12 +11,108 @@ from goodtables import Inspector
 import pandas
 
 from schema_generator import schema_gen
+"""
+Example of goodtables report.
 
-def validation(csv_,cust,file_name,path):
+{
+    "time": 0.025,
+    "valid": false,
+    "error-count": 7,
+    "table-count": 1,
+    "tables": [
+        {
+            "datapackage": "{'name': 'sample_table', 'title': 'sample_table', 'resources': [{'name': 'sample_table', 'path': 'samples/sample_table.csv', 'schema': {'primaryKey': '', 'fields': [{'name': 'key', 'type': 'integer', 'constraints': {'unique': True}}, {'name': 'zip', 'type': 'string'}, {'name': 'tract', 'type': 'integer'}, {'name': 'latitude', 'type': 'number'}, {'name': 'longitude', 'type': 'number'}, {'name': 'soc', 'type': 'string', 'constraints': {'unique': True, 'pattern': '[0-9]{3}-?[0-9]{2}-?[0-9]{4}'}}]}}]}",
+            "time": 0.011,
+            "valid": false,
+            "error-count": 7,
+            "row-count": 6,
+            "source": "./samples/sample_table.csv",
+            "headers": [
+                "key",
+                "zip",
+                "tract",
+                "latitude",
+                "longitude",
+                "soc"
+            ],
+            "format": "inline",
+            "schema": "table-schema",
+            "errors": [
+                {
+                    "code": "pattern-constraint",
+                    "row-number": 2,
+                    "column-number": 6,
+                    "message": "The value \"132-123-1441\" in row 2 and column 6 does not conform to the pattern constraint of \"[0-9]{3}-?[0-9]{2}-?[0-9]{4}\""
+                },
+                {
+                    "code": "pattern-constraint",
+                    "row-number": 3,
+                    "column-number": 6,
+                    "message": "The value \"123-143-1341\" in row 3 and column 6 does not conform to the pattern constraint of \"[0-9]{3}-?[0-9]{2}-?[0-9]{4}\""
+                },
+                {
+                    "code": "unique-constraint",
+                    "row-number": 4,
+                    "column-number": 1,
+                    "message": "Rows 2, 3, 4 has unique constraint violation in column 1"
+                },
+                {
+                    "code": "pattern-constraint",
+                    "row-number": 4,
+                    "column-number": 6,
+                    "message": "The value \"13413441513\" in row 4 and column 6 does not conform to the pattern constraint of \"[0-9]{3}-?[0-9]{2}-?[0-9]{4}\""
+                },
+                {
+                    "code": "type-or-format-error",
+                    "row-number": 5,
+                    "column-number": 3,
+                    "message": "The value \"twelve\" in row 5 and column 3 is not type \"integer\" and format \"default\""
+                },
+                {
+                    "code": "pattern-constraint",
+                    "row-number": 5,
+                    "column-number": 6,
+                    "message": "The value \"124-134-4144\" in row 5 and column 6 does not conform to the pattern constraint of \"[0-9]{3}-?[0-9]{2}-?[0-9]{4}\""
+                },
+                {
+                    "code": "pattern-constraint",
+                    "row-number": 6,
+                    "column-number": 6,
+                    "message": "The value \"4433443\" in row 6 and column 6 does not conform to the pattern constraint of \"[0-9]{3}-?[0-9]{2}-?[0-9]{4}\""
+                }
+            ]
+        }
+    ],
+    "warnings": [],
+    "preset": "datapackage"
+}
+
+"""
+def validation(datapackage,cust,path_and_filename,path):
+    """
+    datapackage: dataset uploaded in datapackage
+        A datapackage is a json like data structure, that contains the original
+        dataset, as well as the schema that was generated previously, e.g.,
+        {
+            "name": filename,
+            "title": filename,
+            "resources": [
+                    {
+                        "name": filename,
+                        "path": path_and_filename,
+                        "schema": schema
+                    }
+                ]
+        }
+
+    cust: eventual housing for custom error messages, NYI
+    path_and_filename: name of the file Uploaded
+    path: directory of the file
+    """
     inspector = Inspector()
     inspector.__init__(row_limit=100000,error_limit=100000) # arbitrary row limit
-    report = inspector.inspect(csv_)
-    email_data = []
+    report = inspector.inspect(datapackage)
+    print(json.dumps(report,indent=4))
     pretty_str = ''
     if path != "":
         path = path + '/'
@@ -25,11 +121,12 @@ def validation(csv_,cust,file_name,path):
         for table in report['tables']:
             s = ast.literal_eval(table['datapackage'])
             filename = s['name'] + "_error_dump.txt"
-            with open(filename,'w',) as fp:
+            with open(path + filename,'w',) as fp:
                 error_rows = []
                 for error in table['errors']:
                     row = error['row-number']
                     error_rows.append(row)
+
                     if 'col' in error.keys():
                         col = error['column-number']
                     else:
@@ -55,10 +152,10 @@ def validation(csv_,cust,file_name,path):
                             value = new_err_str[new_err_str.find("\"")+1:]
                             value = value[:len(value)-1]
                             code = error['code']
-                    pretty_str = pretty_str + err_str + "\n"
-                    email_data.append({'code': code, 'row': row, 'col': col, 'value': value})
-                with open(path + file_name + '_errorlog.csv','w') as sp:
-                    with open(path + file_name + '.csv','r') as rp:
+                            pretty_str = pretty_str + err_str + "\n"
+                            '''
+                with open(path + path_and_filename + '_errorlog.csv','w') as sp:
+                    with open(path + path_and_filename + '.csv','r') as rp:
                         csv_r = csv.reader(rp)
                         csv_w = csv.writer(sp)
                         headers = csv_r.__next__()
@@ -68,24 +165,30 @@ def validation(csv_,cust,file_name,path):
                             if row_number in error_rows:
                                 csv_w.writerow(row)
                             row_number = row_number + 1
-            with open(path + file_name + '_error_report.txt','w') as fp:
-                fp.write(str(table['errors']))
+                            '''
+            with open(path + path_and_filename + '_error_report.json','w') as fp:
+                fp.write(json.dumps(table['errors'],indent=4))
             return table['errors']
     else:
         return "All clear"
-def vali(file_name,desc_file,attachments):
-    filename, file_extension = os.path.splitext(file_name)
+
+
+def vali(path_and_filename,desc_file,attachments):
+    filename, file_extension = os.path.splitext(path_and_filename)
+    assert file_extension == '.csv', 'Please use csv file.'
     f_path = filename.split('/')
     filename = f_path[len(f_path)-1]
     del f_path[len(f_path)-1]
     path = '/'.join(f_path)
-    schema = schema_gen(desc_file,attachments,path) # TODO: Enhance combatibility
-    #custom_error = [{'name': 'phil_zip', 'columns': [], 'message': ' is not a Philadelphia zip code.'},{'name': 'phil_tract', 'columns': [], 'message': ' is not a Philadelphia Census Tract.'},{'name': 'lat', 'columns': [], 'message': ' is not a latitude near Philadelphia.'},{'name': 'lon', 'columns': [], 'message': ' is not a longitude near Philadelphia.'}]
+    schema = schema_gen(desc_file,[],path) # TODO: Enhance combatibility
+    print(json.dumps(schema, indent=4))
+    #custom_error = [{'name': 'ssn', 'columns': [], 'message': ' is not a correctly formatted Social Security Number.'},{'name': 'phil_tract', 'columns': [], 'message': ' is not a Philadelphia Census Tract.'},{'name': 'lat', 'columns': [], 'message': ' is not a latitude near Philadelphia.'},{'name': 'lon', 'columns': [], 'message': ' is not a longitude near Philadelphia.'}]
     # The above is an example of custom error formatting.
-    col_count = 1
-    col_count = col_count + 1
+    # col_count = 1
+    # col_count = col_count + 1
     custom_error = []
-    data_package_json = { "name": filename, "title": filename, "resources": [{"name": filename, "path": file_name, "schema": schema}]}
+    data_package_json = { "name": filename, "title": filename, "resources": [{"name": filename, "path": path_and_filename, "schema": schema}]}
+    print(data_package_json)
     # The csv and schema must be loaded together into a datapackage for use with goodtables
     if file_extension == '.csv':
         return validation(data_package_json,custom_error,filename,path)
